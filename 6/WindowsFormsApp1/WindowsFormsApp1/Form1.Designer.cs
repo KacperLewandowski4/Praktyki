eec7d393893088c80f7a24ac81b4814661d908dd
BotMaks <maksymilian.adamajtis@gmail.com>
2021-08-09 15:25:16 +0200

    z6

---
+++ 6/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.report = new System.Windows.Forms.RichTextBox();
+            this.nextShift = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(127, 31);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(120, 20);
+            this.shifts.TabIndex = 1;
+            this.shifts.ValueChanged += new System.EventHandler(this.shifts_ValueChanged);
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(94, 135);
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(247, 96);
+            this.report.TabIndex = 2;
+            this.report.Text = "";
+            this.report.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // nextShift
+            // 
+            this.nextShift.Location = new System.Drawing.Point(347, 71);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(82, 58);
+            this.nextShift.TabIndex = 3;
+            this.nextShift.Text = "button1";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.shifts);
+            this.groupBox1.Location = new System.Drawing.Point(84, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.report);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.RichTextBox report;
+        private System.Windows.Forms.Button nextShift;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ 6/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex=0;
+            Worker[] workers = new Worker[4];
+            workers[0] = new Worker(new string[] { "Zbieranie nektaru", "Wytwarzanie miodu" });
+            workers[1] = new Worker(new string[] { "Pielęgnacja jaj", "Nauczanie pszczółek" });
+            workers[2] = new Worker(new string[] { "Utrzymywanie ula", "Patrol z żądłami" });
+            workers[3] = new Worker(new string[] { "Zbieranie nektaru", "Wytwarzanie miodu", "Pielęgnacja jaj", "Nauczanie pszczółek","Utrzymywanie ula","Patrol z żądłami" });
+            queen = new Queen(workers);
+
+        }
+        private Queen queen;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (queen.AssignWork(comboBox1.Text, (int)shifts.Value) == false)
+                MessageBox.Show("Nie ma dostępnych robotnic do wykonania zadania '" + comboBox1.Text + "'", "Królowa pszczół mówi...");
+            else
+                MessageBox.Show("Zadanie '" + comboBox1.Text + "' będzie ukończone za " + shifts.Value + " zmiany", "Królowa pszczół mówi...");
+
+        }
+
+        private void shifts_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nextShift_Click(object sender, EventArgs e)
+        {
+            report.Text = queen.WorkTheNextShift();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
