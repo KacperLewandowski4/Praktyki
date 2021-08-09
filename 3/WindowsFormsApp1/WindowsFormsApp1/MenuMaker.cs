a8de663cc6db08e0a3af0e87b70998bc116cce2d
BotMaks <maksymilian.adamajtis@gmail.com>
2021-08-09 12:13:03 +0200

    z3

---
+++ 3/WindowsFormsApp1/WindowsFormsApp1/MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "wołowina", "salami", "indyk" };
+        string[] Condiments = { "musztarda", "majonez", "ketchup" };
+        string[] Breads = { "chleb", "bułka" };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + "," + randomCondiment + "," + randomBread;
+        }
+    }
+
+}
